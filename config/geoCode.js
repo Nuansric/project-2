3fa7984c66f5e142f49f117e61bfea16c99c4568
Kashan Ali Qazi <kashan.qazi02@utexas.edu>
2017-02-22 22:16:45 -0600

    added code for geocode.js

--- config/geoCode.js
+++ config/geoCode.js
@@ -1,26 +1,49 @@
 //AJAX Call to google map API
 
+// installing request npm package to receive data from URL
 var request = require("request");
 
-var apiKey = "AIzaSyCDuVHudRelXYb1Sba9VCXt51P-zPFowQE";
+var APIKeys = require("../keys.js");
 
+// API key for google.
+var googleCred = APIKeys.keys.googleApi;
+// URL where the request for geocoding is made
 var geoCodeURL = "https://maps.googleapis.com/maps/api/geocode/json?address=";
 
-var geocode = function(address) {
+// pass the address and call back function to geocode
+// call back function waits for the results from request URL
+// and stores the value in a variable as an object
+var geocode = function(address, cb) {
 
-    var url = geocodeURL + address + "&key=" + apiKey;
+    // concatenate different parts of URL to make a query
+    var url = geoCodeURL + address + "&key=" + apiKey;
 
-    console.log(url);
-
+    //call the request function. Call back function extracts the relevant data
+    // in this case, longitude and latitude associate with hte address
     request(url, function(error, response, body){
 
-        console.log(response);
+        // convert the body to JSON. Body is a string originally
+        var result = JSON.parse(body);
+
+        // store the values in a variable
         var coordinates = {
-            latitude: response.results.geometry.location.lat,
-            longitude: response.results.geometry.location.lon
+            latitude: result.results[0].geometry.location.lat,
+            longitude: result.results[0].geometry.location.lng
         }
-    return coordinates;
-    });
+
+        // cb function helps storing the value outside the callback function
+        // for the request query.
+        cb(coordinates);
+
+    });   
 };
+
+// call the geocode function, along with the callback function
+var latLng = geocode(address, function(res){
+
+    var coordinates = res;
+    return coordinates;
+
+});
 
 module.exports = geocode;
