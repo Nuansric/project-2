5e05413cce1a67e592bb2744311829942496edf7
Chanita Nuansri <nuansric@Chanitas-MacBook-Pro.local>
2017-02-22 19:00:10 -0600

    server.js updated with handlebars stuff

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -1,3 +1,19 @@
-var addServiceController = require("../controller/addService_controller");
+var addServiceController = require("../controller/search_controller");
 
 //export all routes into server.js
+module.exports = function(app){
+
+
+
+// 	app.get("/login", function(req, res) {
+
+//     res.sendFile(path.join(__dirname + "/../public/login.html"));
+// });	
+
+app.get("/search", addServiceController.renderSearchBar);
+
+
+
+
+
+}
--- server.js
+++ server.js
@@ -23,8 +23,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var db = require("./models");
+var methodOverride = require("method-override");
 
-// Sets up the Express App
+
+
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -37,11 +39,18 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Static directory
  app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+// Sets up the Express App
 
 // Routes
 // =============================================================
 require("./routes/signupLoginRoutes.js")(app);
+require("./routes/searchRoutes.js")(app);
 
 
 db.sequelize.sync({ force: true }).then(function() {
