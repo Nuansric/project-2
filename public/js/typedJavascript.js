9f2518221b72b64227bf62de4cd401e97f1b442d
Chanita Nuansri <nuansric@Chanitas-MacBook-Pro.local>
2017-03-02 17:07:32 -0600

    replaced the overrode file

---
+++ public/js/typedJavascript.js
@@ -0,0 +1,24 @@
+$(function(){
+
+        $("#typed").typed({
+            strings: ["a plumber?", "a real estate agent?", "a tutor?", "a dog walker?"],
+            stringsElement: $('#typed-strings'),
+            typeSpeed: 30,
+            backDelay: 500,
+            loop: false,
+            contentType: 'html', // or text
+            // defaults to false for infinite loop
+            loopCount: false,
+            callback: function(){ foo(); },
+            resetCallback: function() { newTyped(); }
+        });
+
+        $(".reset").click(function(){
+            $("#typed").typed('reset');
+        });
+
+    });
+
+    function newTyped(){ /* A new typed object */ }
+
+    function foo(){ console.log("Callback"); }
